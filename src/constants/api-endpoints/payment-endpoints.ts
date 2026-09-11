0e919a99ee544965a7516b924efa07e57ea9e984
rishadsinu <rishadsinu2233@gmail.com>
2026-09-11 16:20:28 +0530

    chores

--- src/constants/api-endpoints/payment-endpoints.ts
+++ src/constants/api-endpoints/payment-endpoints.ts
@@ -1,18 +1,3 @@
-// export const PAYMENT_ENDPOINTS = {
-//     CREATE_ORDER: "/payment/create-order",
-//     VERIFY_PAYMENT: "/payment/verify",
-//     WORK_COMPLETED: "/payment/work-completed",
-
-//     WALLET: "/payment/wallet",
-
-//     ADMIN: {
-//         SUMMARY: "/payment/admin/summary",
-//         PAYMENTS: "/payment/admin/payments",
-//         PAYMENT_STATS: "/payment/admin/payment-stats",
-//     },
-//     WORKER_EARNINGS_STATS: "/payment/worker/earnings-stats",
-// };
-
 export const PAYMENT_ENDPOINTS = {
     CREATE_ORDER: "/payment/create-order",
     VERIFY_PAYMENT: "/payment/verify",
